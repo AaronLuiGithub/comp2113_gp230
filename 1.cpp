b287aed255cdc7418ab4f426c3c8fdd8a9b1c1ba
nathanlam650 <63201411+nathanlam650@users.noreply.github.com>
2020-04-28 14:30:04 +0800

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,75 @@
+//The game will have 3 level, 2*2,3*3,4*4
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <math.h>
+
+
+using namespace std;
+
+void printing(bool *x,int level){//maybe can upgrade? I will do it later
+	int sqrlevel = sqrt(level); 
+	for (int i=0; i<level; ++i){
+		if (x[i]== true)  cout<<"1"; 
+		else cout<<"0";
+		if ((i%sqrlevel)==(sqrlevel-1))	cout << endl;
+	}
+}
+
+int getlevel(){//get level, to be finish
+	int level = 2;
+	return level;
+}
+
+void input(bool *x2,int level){//to be finish
+	char temp;
+	for(int i=0;i<level;++i){
+		cin >> temp;
+		if (temp=='1') x2[i]=true;
+		else if (temp=='0') x2[i]=false;
+		else {
+			cout << "error input, please input again";
+			input(x2, level);
+		}
+	}
+}
+
+int main(){
+	srand(time(NULL) );
+	int level;
+	level= getlevel();
+	level = level*level;
+	bool *x = new bool [level];//I know how to use 2d dynamic array but I don't want to use it.
+	bool *x2 = new bool [level];
+	
+	for (int i=0; i<level; ++i){  //rendom out picture
+		if ((rand()%2)>0.5)  x[i]=true;
+		else  x[i]=false;
+	}
+	
+	printing(x,level);
+	//sleep(1200);
+	cout<<"press enter to start answer";
+	getchar();
+	system("cls||clear");//to be improve...
+	
+	input(x2,level);
+	
+	bool win=true;
+	for (int i=0; i<level; ++i){  //rendom out picture
+		if (x2[i]!=x[i])  {
+			cout <<"wrong answer, you are weak.";
+			win=false;
+		}
+	}
+	if (win==true) cout << "correct answer, you are strong.";
+	
+	delete[]x;
+	delete[]x2;
+	
+	return 0;
+	
+}
+
+
