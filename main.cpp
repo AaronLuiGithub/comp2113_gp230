24d4d186a7c3c75b1da9c263abec322b25e2b6bb
nathanlam650 <63201411+nathanlam650@users.noreply.github.com>
2020-05-07 03:43:39 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,19 +18,25 @@ int main()
    cout << "what do you want to do?" << endl;
    cout << "Enter: " << endl;    
    cout << "1 for play Visual memory mode " << endl;
-   cout << "2 for Verbal memory mode " << endl; 
-   cout << "3 for visit past score " << endl; 
+   cout << "2 for play Verbal memory mode " << endl; 
+   cout << "3 for visit and record past score and then quit" << endl; 
    cout << "4 for qiut " << endl;
+   cout << "p.s. you may play again and again but only latest score will be recort " << endl;  
    
    int n;
+  int scores
    while (true){
        cin >> n;
        switch(n) {
            case 1:
+           scores = visal(); 
            
            case 2:
+           scores = verbal();
            
            case 3:
+           score(scores);
+           break;
            
            case 4:
            break;
